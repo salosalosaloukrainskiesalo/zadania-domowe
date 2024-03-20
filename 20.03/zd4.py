41b3ef7a36241be570e9ea35bbd92ffcf9010cb2
Illia <killerttere.com>
2024-03-20 19:01:49 +0100

    test

---
+++ 20.03/zd4.py
@@ -0,0 +1,18 @@
+a = "Common Sequence Operation"
+l = list(a)
+a2 = '-'.join(a[6:15]) 
+print(a2)
+
+
+b= "Mutable Sequence Types"
+print(b[-5:])
+
+c = "Mutable Sequence Types"
+c2 = c[1::2]
+print(c2)
+
+
+d = "Mutable Sequence Types"
+t = d.split()
+d2 = " ".join(t[::-1])
+print(d2)
